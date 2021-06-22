bbf8352a36b20dedd7caeed9396773c3636b564a
MRea <rotavele666@pm.me>
2021-06-21 21:12:14 -0700

    penultimate commit

--- flask_app/controllers/log_reg_ctrl.py
+++ flask_app/controllers/log_reg_ctrl.py
@@ -7,11 +7,54 @@ bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
-    info = User.get_all()
-    return render_template('index.html', all_email = info)
+    return render_template('index.html')
 
-@app.route('/sign_up/', methods = ['POST'])
+
+@app.route('/sign_up',methods =['POST'])
 def sign_up():
-    if not User.validate_email(request.form):
+    if not User.validate_signup(request.form):
         return redirect('/')
     
+    hash_me = bcrypt.generate_password_hash(request.form['password'])
+    
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'birthdate': request.form['birthdate'],
+        'password': hash_me
+    }
+    user_id = User.create(data)
+    session['client'] = user_id
+    
+    return redirect('/success/signup')
+
+    
+@app.route('/login',methods =['POST'])
+def login():
+    if not User.validate_login(request.form):
+        return redirect('/')
+    
+    user = User.get_by_email({ "email": request.form['email']})
+    session['client'] = user.id
+
+    return redirect('success/login')
+    
+    
+@app.route('/success/signup')
+def reg_success():
+    if 'client' not in session:
+        return redirect('/')
+    return render_template('success_reg.html', user = User.get_by_id({"id": session['client']}))
+
+
+@app.route('/success/login')
+def log_success():
+    return render_template('success_log.html', user = User.get_by_id({"id": session['client']}))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
--- flask_app/models/log_reg_mdl.py
+++ flask_app/models/log_reg_mdl.py
@@ -1,7 +1,10 @@
 from ..config.mysqlconnections import connectToMySQL
 from flask import flash
 import re
+from flask_app import app
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt(app)
 
 class User:
     def __init__(self, data):
@@ -11,49 +14,85 @@ class User:
         self.email = data['email']
         self.birthdate = data['email']
         self.password = data['password']
-        self.password_confirm = data['password_confirm']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        
-        
+
+
     @classmethod
     def get_all(cls):
-        query = 'SELECT * FROM users;'
-        results = connectToMySQL('login_registration_schema').query_db(query)
-        clients = []
+        pass
+    
+    
+    @classmethod
+    def create(cls,data):
+        query = "INSERT INTO users (first_name, last_name, email, birthdate, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(birthdate)s, %(password)s, NOW(), NOW());"
         
-        for row in results:
-            clients.append(User(row))
-        return clients
+        results = connectToMySQL('login_registration_schema').query_db(query, data)
+        return results
+
+
+    @classmethod
+    def get_by_email(cls,data):
+        query = 'SELECT * FROM users WHERE email = %(email)s;'
+
+        results = connectToMySQL('login_registration_schema').query_db(query, data)
+        if len(results) < 1:
+            return False
+        return User(results[0])
+
+
+    @classmethod
+    def get_by_id(cls,data):
+        query = 'SELECT * FROM users WHERE id = %(id)s;'
+
+        results = connectToMySQL('login_registration_schema').query_db(query, data)
+        return User(results[0])
+
+
+    @classmethod
+    def update_edit():
+        pass
 
 
     @staticmethod
-    def validate_registration(post_data):
+    def validate_signup (post_data):
         is_valid = True
         if len(post_data['first_name']) < 2:
-            flash("First name must be at least two characters.")
+            flash("First Name must be at least two characters.")
             is_valid = False
-            
+
         if len(post_data['last_name']) < 2:
             flash("Last Name must be at least two characters.")
             is_valid = False
-            
-        if len(post_data['birthdate']) < 2:
-            flash("You must be at least 10 years old to register.")
-            is_valid =  False
-            
-        if len(post_data['password']) < 1:
-            flash("Yo.")
-            is_valid = False
-            
-        return is_valid
 
-    @staticmethod
-    def validate_email(post_data):
-        is_valid = True
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(post_data['email']):
             flash("Email is not valid.")
             is_valid = False
-            
+
+        if len(post_data['birthdate']) < 2:
+            flash("You must be at least 10 years old to register.")
+            is_valid =  False
+
+        if len(post_data['password']) < 4:
+            flash("Password must longer.")
+            is_valid = False
+        else:
+            if post_data['password'] != post_data['confirm_password']:
+                flash('Passwords do not match.')
+                is_valid = False
+
         return is_valid
+
+
+    @staticmethod
+    def validate_login (post_data):
+        user_from_db = User.get_by_email({'email': post_data['email']})
+        if not user_from_db:
+            flash('invalid credentials')
+            return False
+        if not bcrypt.check_password_hash(user_from_db.password,post_data['password']):
+            flash('Invalid Credentials.')
+            return False
+        
+        return True
